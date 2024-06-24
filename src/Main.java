4a4844aebc7c7e7ef31e3c02f78654e724063076
Javid <sairexm@gmail.com>
2024-06-24 18:52:58 +0400

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //TASK 1
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+
+        //TASK 2
+        System.out.println(fullName.toUpperCase());
+
+        //TASK 3
+        String firstNameToFix = "Семён";
+        String lastNameToFix = "Иванов";
+        String middleNameToFix = "Семёнович";
+        firstNameToFix = firstNameToFix.replace("ё", "е");
+        lastNameToFix = lastNameToFix.replace("ё", "е");
+        middleNameToFix = middleNameToFix.replace("ё", "е");
+        String fullNameToFix = lastNameToFix + " " + firstNameToFix + " " + middleNameToFix;
+        System.out.println(fullNameToFix);
     }
 }
